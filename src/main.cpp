155c26cc5df61497e5a864ee444acd40ff6d812a
Dallas Epperson <depperson@pcgus.com>
2020-01-19 17:09:19 -0500

    SFML compile test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-#include "SFML/Graphics.hpp"
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-int main(){
-    std::cout << "Is this working?" << "\n";
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
